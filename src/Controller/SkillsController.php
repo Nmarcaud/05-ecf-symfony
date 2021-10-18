00d116d0805272941043da48d53089374ecbe2c6
Nicolas marcaud <nmarcau@gmail.com>
2021-10-18 21:53:20 +0200

    Adding SkillType

--- src/Controller/SkillsController.php
+++ src/Controller/SkillsController.php
@@ -3,13 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Skill;
+use App\Form\SkillType;
 use App\Repository\SkillRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -37,30 +37,11 @@ class SkillsController extends AbstractController
     /**
      * @Route("/create", name="skills_create")
      */
-    public function create(FormFactoryInterface $factory, Request $request, EntityManagerInterface $em): Response
+    public function create(Request $request, EntityManagerInterface $em): Response
     {
-
-        // Form d'ajout & utilisation de data_class
-        $builder = $factory->createBuilder(FormType::class, null, [
-            'data_class' => Skill::class
-        ]);
+        // Form d'ajout
+        $form = $this->createForm(SkillType::class);
         
-        // Les champs
-        $builder->setMethod('POST')
-            ->add('name', TextType::class, [
-                'label' => 'Nom de la compétence',
-                'attr' => [
-                    'placeholder' => 'Tapez le nom de la compétence'
-                ]
-            ])
-            ->add('submit', SubmitType::class, [
-                'label' => 'Ajouter la compétence'
-            ]);
-
-        // On récupère le form ( mias la classe est énorme )
-        $form = $builder->getForm();
-        
-
         // Regarde dans la request si quelque chose t'intéresse
         $form->handleRequest($request);
 
@@ -75,8 +56,6 @@ class SkillsController extends AbstractController
             
             return $this->redirectToRoute('skills', [], Response::HTTP_SEE_OTHER);
         }
-
-    
 
         // Récupère juste la partie visualisation
         $formView = $form->createView();
